83f334cf3c2a4f4d2a96cb7812cf5c1c92ac3d75
tomtom99lol <thomas.otool98@gmail.com>
2024-05-29 17:50:01 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,94 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const db = require('./server/db');
+const cors = require('cors');
+
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cors());
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+});
+  
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/register', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+app.get('/products', db.getProducts);
+app.get('/products/:id', db.getProductById);
+app.post('/products', db.createProduct);
+
+app.get('/carts/:id', db.getCartById);
+app.post('/carts/:id', db.addProductToCart);
+app.get('/carts/:id/checkout', db.checkoutCart, db.addCartToOrder);
+
+app.get('/orders', db.getAllOrders);
+app.get('/orders/:id', db.getOrderById);
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(
+  session({
+    secret: 'bang',
+    resave:false,
+    saveUninitialized:false
+  })
+);
+
+app.use(passport.initialize());  
+app.use(passport.session());
+
+// Set method to serialize data to store in cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Set method to deserialize data stored in cookie and attach to req.user
+passport.deserializeUser((id, done) => {
+  done(null, { id });
+});
+
+passport.use(new LocalStrategy(
+     function (username, password, done) {
+        db.pool.query(
+            'SELECT * FROM users WHERE name = $1', [username],
+            (error, result) => {
+                if (error){
+                    return done(error);
+                }
+                if (!result.rows[0]){  
+                    return done(null, false);
+                }
+                if (result.rows[0].password != password){
+                    return done(null, false);
+                }
+                return done(null, result.rows[0]);
+            }
+        )
+    }
+));
+
+app.post(
+  '/login',
+  passport.authenticate('local', {failureRedirect: '/'}),
+  (req, res) => {res.redirect(`/users/${req.user.id}`)}
+);
+
+/*app.get('/profile', (req, res) =>{
+  res.json()
+})*/
 
 app.listen(4001, () =>{
     console.log('Port 4001 is up and running');
---
+++ server/db.js
@@ -0,0 +1,242 @@
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ecommerce-app',
+  password: 'postgres',
+  port: 5432,
+})
+
+const getUsers = (request, response) => {
+	pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+	  if (error) {
+		throw error
+	  }
+	  response.status(200).json(results.rows)
+	})
+};
+
+const getUserById = (request, response) => {
+	const id = parseInt(request.params.id)
+  
+	pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+	  if (error) {
+		throw error
+	  }
+	  response.status(200).json(results.rows)
+	})
+};
+
+const createUser = (request, response) => {
+	const { name, email, password  } = request.body;
+  
+	pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', 
+	[name, email, password], (error, results) => {
+	  if (error) {
+		throw error
+	  }
+	  response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+	})
+};
+
+const updateUser = (request, response) => {
+	const id = parseInt(request.params.id)
+	const { name, email, password } = request.body;
+  
+	pool.query(
+	  'UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4',
+	  [name, email, password, id],
+	  (error, results) => {
+		if (error) {
+		  throw error
+		}
+		response.status(200).send(`User modified with ID: ${id}`)
+	  }
+	)
+};
+
+const deleteUser = (request, response) => {
+	const id = parseInt(request.params.id)
+  
+	pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+	  if (error) {
+		throw error
+	  }
+	  response.status(200).send(`User deleted with ID: ${id}`)
+	})
+};
+
+const getProducts = (req, res) =>{		
+		if (!req.query.category){
+			pool.query('SELECT * FROM products ORDER BY id ASC', (err, results) =>{
+				if (err) {
+					throw err
+				}
+				res.status(200).send(results.rows);
+		})} else {
+			const {category} = req.query;
+			pool.query('SELECT * FROM products WHERE category = $1 ORDER BY id ASC', [category], (err, results) =>{
+				if (err){
+					throw err;
+				}
+				res.status(200).send(results.rows);
+		})}
+}
+	
+const getProductById = (req, res) =>{
+	const id = parseInt(req.params.id);
+	pool.query('SELECT * FROM products WHERE id = $1', [id], (err, results) =>{
+		if (err){
+			throw err
+		}
+		res.status(200).send(results.rows)
+	})
+}
+
+const createProduct = (req, res) =>{
+	const {name, price, category} = req.body;
+	pool.query('INSERT INTO products (name, price, category) VALUES ($1, $2, $3) RETURNING *', 
+	[name, price, category], (err, results) =>{
+		if (err){
+			throw err
+		}
+		res.status(200).send(`Created product with ID: ${results.rows[0].id}`)
+	})
+};
+
+const getCartById = (req, res) =>{
+	const id = parseInt(req.params.id);
+	pool.query('SELECT * FROM carts WHERE id = $1', [id], (err, results) =>{
+		if (err){
+			throw err;
+		}
+		res.status(200).send(results.rows);
+	})
+}
+
+const addProductToCart = (req, res) =>{
+	const id = parseInt(req.params.id);
+	const {product1_id, product2_id, product3_id} = req.body;
+	pool.query('INSERT INTO carts (id, product1_id, product2_id, product3_id) VALUES ($1, $2, $3, $4)',
+	[id, product1_id, product2_id, product3_id], (err, results) =>{
+		if (err){
+			throw err;
+		}
+		res.status(200).send(`Products added to cart with ID: ${id}`)
+	})
+}
+
+//const messages = [];
+//res.locals.msg = [];
+const checkoutCart = (req, res, next) =>{
+	const id = parseInt(req.params.id);
+	pool.query('SELECT product1_id, product2_id, product3_id FROM carts WHERE id = $1', [id], async(err, results) =>{
+		if (err){
+			throw err;
+		}
+		
+		const getPrice1 = () =>{return new Promise((resolve,reject) =>{
+			pool.query('SELECT * FROM products WHERE id = $1', [results.rows[0].product1_id], (err, Result) =>{
+				if (err){
+					return reject(err);
+				}
+				
+				const price1 = Result.rows[0].price;
+				return resolve(price1);
+			})
+		})};
+
+		const getPrice2 = () =>{return new Promise((resolve,reject) =>{
+			pool.query('SELECT * FROM products WHERE id = $1', [results.rows[0].product2_id], (err, Result) =>{
+				if (err){
+					return reject(err);
+				}
+				
+				const price2 = Result.rows[0].price;
+				return resolve(price2);
+			})
+		})};
+
+		const getPrice3 = () =>{return new Promise((resolve,reject) =>{
+			pool.query('SELECT * FROM products WHERE id = $1', [results.rows[0].product3_id], (err, Result) =>{
+				if (err){
+					return reject(err);
+				}
+				
+				const price3 = Result.rows[0].price;
+				return resolve(price3);
+			})
+		})};
+
+		const getTotalPrice = async () =>{
+			let totalPrice;
+			const price1 = await getPrice1();
+			const price2 = await getPrice2();
+			const price3 = await getPrice3();
+
+			totalPrice =  await (Number(price1) + Number(price2) + Number(price3));
+			
+			return totalPrice;
+		}
+		const totalPrice = await getTotalPrice();
+		
+		//console.log(totalPrice);
+		res.locals.msg = [`Your total is ${totalPrice}`];
+		next();
+	}) 
+}
+
+const addCartToOrder = (req, res, next) =>{
+	const id = parseInt(req.params.id);
+	pool.query(
+		'WITH prev AS(SELECT * FROM carts WHERE id = $1) \
+		INSERT INTO orders (user_id, product1_id, product2_id, product3_id) \
+		SELECT prev.id, prev.product1_id, prev.product2_id, prev.product3_id FROM prev \
+		RETURNING *', [id],
+		(err, results) =>{
+			if (err){
+				throw err;
+			}
+			res.locals.msg.push(`Order created with ID: ${results.rows[0].id} fpr user with ID: ${id}`);
+			res.status(200).send(res.locals.msg.join('\n'));
+		} 
+	)
+}
+
+const getAllOrders = (req, res) =>{
+	pool.query('SELECT * FROM orders ORDER BY id ASC', (err, results) =>{
+		if (err){
+			throw err;
+		}
+		res.status(200).json(results.rows);
+	})
+}
+
+const getOrderById = (req, res) =>{
+	const id = parseInt(req.params.id);
+	pool.query('SELECT * FROM orders WHERE id = $1', [id], (err, results) =>{
+		if (err){
+			throw err;
+		}
+		res.status(200).json(results.rows[0]);
+	})
+}
+
+module.exports = {
+	getUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+	getProducts,
+	getProductById,
+	createProduct,
+	getCartById,
+	addProductToCart,
+	checkoutCart,
+	addCartToOrder,
+	getAllOrders,
+	getOrderById,
+	pool
+};
